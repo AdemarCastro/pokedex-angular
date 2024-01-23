903da5bffedc99806821ef4e4030be2930794ea2
AdemarCastro <ademar.castro.fh@gmail.com>
2024-01-23 14:05:20 -0400

    [FEAT] - Adiciona o 'app.module.ts' e novas configurações de rotas

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HomeModule} from "./home/home.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {RouterModule} from "@angular/router";
+import {routes} from "./app.routes";
+import {AppComponent} from "./app.component";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+})
+export class AppModule {
+
+}
