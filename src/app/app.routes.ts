714b1ec007502f8688a29727b6801518d5092f73
AdemarCastro <ademar.castro.fh@gmail.com>
2024-01-23 13:41:57 -0400

    [FEAT] - Configura as rotas do 'app.routes.ts'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'A. Castro - Pokedex',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((h) => h.HomeModule),
+  }
+];
