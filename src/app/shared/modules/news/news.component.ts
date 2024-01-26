14525e1de261180e6c28a629b79642c62e8dac0c
AdemarCastro <ademar.castro.fh@gmail.com>
2024-01-26 14:22:24 -0400

    [FIX] - Corrigi um bug relacionado ao ciclo de vida do Angular [FIX] - Corrigi o acionamento da funcionalidade vibrar ao iniciar a página [REFACTOR] - Aprimora a lógica do componente 'news'

--- src/app/shared/modules/news/news.component.ts
+++ src/app/shared/modules/news/news.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {NewsService} from "../../../services/news.service";
 import {NewsInterface} from "../../interfaces/news.interface";
 import {DateUtils} from "../../utils/date.utils";
@@ -19,16 +19,19 @@ export class NewsComponent implements OnInit, AfterViewInit{
   startIndex: number = 0;
   chunkSize: number = 10;
 
+  constructor(
+    private newsService: NewsService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
   ngOnInit(): void {
     this.searchNews('Pokemon');
   }
-
   // Exibir o componente apenas quando for totalmente carregado
-  ngAfterViewInit() : void {
-    this.loading = false;
-  }
 
-  constructor(private newsService: NewsService) {}
+  ngAfterViewInit() : void {
+    this.cdr.detectChanges();
+  }
 
   searchNews(query: string): void {
     this.loading = true;
@@ -36,28 +39,21 @@ export class NewsComponent implements OnInit, AfterViewInit{
     this.newsService.getNews(query).subscribe({
       next: (data) => {
         this.news = data.articles;
+        console.log(this.news);
         return this.toTraverseNews();
-        // console.log(this.news);
       },
       error: (error) => {
         console.log('Erro ao buscar notícias', error);
       },
       complete: () => {
         console.log('Busca de notícias completa');
+        this.loading = false;
       }
     });
   }
 
   // To traverse News
   toTraverseNews() {
-
-    // Condição que vibra o dispositivo ao clicar no botão, apenas para dispositivos que possuem tal função
-    // Pattern captura a quantidade de milissegundos em que o dispositivo irá vibrar
-    // Também é possível passar um parâmetro em forma de array como [200, 100, 200] para que o dispositivo vibre por 200ms, pause por 100ms e vibre novamente por 200ms
-    if ('vibrate' in navigator) {
-      navigator.vibrate(100);
-    }
-
     this.allNews = this.allNews.concat(this.news.slice(this.startIndex, this.startIndex + this.chunkSize).map((n) => {
       return {
         title: n.title,
@@ -69,6 +65,15 @@ export class NewsComponent implements OnInit, AfterViewInit{
     }));
 
     this.startIndex += this.chunkSize;
+  }
+
+  vibrate() {
+    // Condição que vibra o dispositivo ao clicar no botão, apenas para dispositivos que possuem tal função
+    // Pattern captura a quantidade de milissegundos em que o dispositivo irá vibrar
+    // Também é possível passar um parâmetro em forma de array como [200, 100, 200] para que o dispositivo vibre por 200ms, pause por 100ms e vibre novamente por 200ms
+    if ('vibrate' in navigator) {
+      navigator.vibrate(100);
+    }
   }
 
   hasMoreNews(): boolean {
