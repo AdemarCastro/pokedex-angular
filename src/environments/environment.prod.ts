d06dd30505d6dfa00d435360892c7d658cae9fb6
AdemarCastro <ademar.castro.fh@gmail.com>
2024-01-27 14:24:11 -0400

    [FIX] - Adiciona o arquivo 'environment.prod.ts' ao projeto, corrigindo o bug de produção por não conseguir encontrá-lo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  /*apiKey: '2d14116f687e4494b17025c194eff8a4' ademar.castro.fh@gmail.com*/
+  apiKey: '28d5757ea4114595becfb5149d243285' /* ademar.a.castro.fh@gmail.com */
+}
